20fd110751edcda2f7b6e31b8148fc837954d585
JovaneJames <jovanekjames@icloud.com>
2022-04-23 09:41:51 +0100

    targets the selection element -for epl fixtures - then loops through each article element inside the section element, finally targeting competing team names and their odds via their class name

--- web_connection_module.py
+++ web_connection_module.py
@@ -4,10 +4,12 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 
+
 class ConnectToWebsite:
 
     def __init__(self):
-        self.website_url = 'https://sports.williamhill.com/betting/en-gb/football/matches/competition/today/match-betting'
+        self.website_url = 'https://sports.williamhill.com/betting/en-gb/football/matches/competition/today/match' \
+                           '-betting '
         self.chrome_options = Options()
         # chrome_options.binary_location = browser_location
         self.chrome_options.add_argument("--headless")  # Ensure GUI is off
@@ -20,6 +22,21 @@ class ConnectToWebsite:
         self.driver.get(self.website_url)
         cookie_button = self.driver.find_element(by=By.CLASS_NAME, value="cookie-disclaimer__button")
         cookie_button.click()
+        self.get_epl_fixtures()
+
+    def get_epl_fixtures(self):
         epl_section = self.driver.find_element(by=By.XPATH, value="//*[@id=\"dml\"]/div/div[2]/section[1]")
-        print(epl_section.text)
+        row_events = epl_section.find_elements(by=By.XPATH,
+                                               value="//*[@id=\"dml\"]/div/div[2]/section[1]/div/div/article")
+        self.extract_team_names_and_odds(row_events)
         self.driver.quit()
+
+    @staticmethod
+    def extract_team_names_and_odds(row_events):
+        for row in row_events:
+            teams_playing = row.find_elements(by=By.CLASS_NAME, value="sp-betName")
+            team_odds = row.find_elements(by=By.CLASS_NAME, value="sp-o-market__buttons")
+            for fixture in teams_playing:
+                print(fixture.text)
+            for odds in team_odds:
+                print(odds.text)
