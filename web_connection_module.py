1a1b71871a1d3ef12c72a23b1ad3fa06dbec85fd
JovaneJames <jovanekjames@icloud.com>
2022-04-24 06:11:40 +0100

    refactoring regarding how fixtures and corresponding odds are webscraped and stored in a dict

--- web_connection_module.py
+++ web_connection_module.py
@@ -12,11 +12,15 @@ class ConnectToWebsite:
                            '-betting '
         self.chrome_options = Options()
         # chrome_options.binary_location = browser_location
-        self.chrome_options.add_argument("--headless")  # Ensure GUI is off
+        self.chrome_options.browser_version = '100.0.4896.127'
+        self.chrome_options.headless = True
+        # self.chrome_options.add_argument("--headless")  # Ensure GUI is off
         self.chrome_options.add_argument("--no-sandbox")
         self.chromedriver_path = './chromedriver'
         self.webdriver_service = Service(self.chromedriver_path)
         self.driver = webdriver.Chrome(service=self.webdriver_service, options=self.chrome_options)
+        self.listofteams = []
+        self.listofOdds = []
 
     def established_connection(self):
         self.driver.get(self.website_url)
@@ -25,18 +29,13 @@ class ConnectToWebsite:
         self.get_epl_fixtures()
 
     def get_epl_fixtures(self):
-        epl_section = self.driver.find_element(by=By.XPATH, value="//*[@id=\"dml\"]/div/div[2]/section[1]")
-        row_events = epl_section.find_elements(by=By.XPATH,
-                                               value="//*[@id=\"dml\"]/div/div[2]/section[1]/div/div/article")
-        self.extract_team_names_and_odds(row_events)
-        self.driver.quit()
-
-    @staticmethod
-    def extract_team_names_and_odds(row_events):
+        row_events = self.driver.find_elements(by=By.XPATH, value="//*[@id=\"dml\"]/div/div[2]/section/div/div/article")
         for row in row_events:
             teams_playing = row.find_elements(by=By.CLASS_NAME, value="sp-betName")
             team_odds = row.find_elements(by=By.CLASS_NAME, value="sp-o-market__buttons")
-            for fixture in teams_playing:
-                print(fixture.text)
+            for fixtures in teams_playing:
+                self.listofteams.append(fixtures.text)
             for odds in team_odds:
-                print(odds.text)
+                self.listofOdds.append(tuple(odds.text.split('\n')))
+        dict_from_list = dict(zip(self.listofteams, self.listofOdds))
+        print(dict_from_list)
